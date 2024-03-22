e473306605941255ea653c0c3c6edb36366a92a4
Method Mkoma <gershommethod02@gmail.com>
2024-03-23 00:23:59 +0300

    Task 2: New

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# a manifest that kills a process named killmenow.
+exec {'pkill_killmenow':
+  command     => '/usr/bin/pkill killmenow',
+  path        => '/alx-system_engineering-devops/0x0A-configuration_management',
+  refreshonly => true,
+  onlyif      => '/bin/pgrep killmenow',
+}
