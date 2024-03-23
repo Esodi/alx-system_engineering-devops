8bddabac0af16e1dfc2dba5f1d25b6e7716a4b04
Method Mkoma <gershommethod02@gmail.com>
2024-03-23 10:57:53 +0300

    Task 2: Fixing

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
 #!/usr/bin/pup
 # a manifest that kills a process named killmenow.
 exec {'pkill_killmenow':
-  command     => '/usr/bin/pkill killmenow',
-  path        => '/alx-system_engineering-devops/0x0A-configuration_management',
-  refreshonly => true,
-  onlyif      => '/bin/pgrep killmenow',
+  command  => '/usr/bin/pkill killmenow',
+  provider => 'shell',
 }
