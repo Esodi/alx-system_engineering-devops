ab7968afce0631b2a07373e4580ee9545ff8aafd
Method Mkoma <gershommethod02@gmail.com>
2024-03-23 10:42:49 +0300

    Task 1: Fixing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,11 +3,4 @@
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  notify   => Exec['echo_newline'],
-}
-
-exec { 'echo_newline':
-  command     => 'echo -e "\n"',
-  path        => '/usr/bin',
-  refreshonly => true,
 }
