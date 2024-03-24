3057e25d7b686bc30890e8d1e74fff4a9df919f7
Method Mkoma <gershommethod02@gmail.com>
2024-03-24 23:12:18 +0300

    Task 4: Client configuration file (w/ Puppet) - Fixing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,13 @@
 #!/usr/bin/pup
 # make changes to the ssh configuration file.
 file {~/.ssh/config:
-  ensure       => present,
-  mode         => 700,
-  Host         => remote_host,
-  user         => ubuntu,
-  HostName     => 18.207.207.117,
-  IdentifyFile => ~/.ssh/school
+  ensure  => present,
+  mode    => '0700',
+  content => "
+    Host remote_host,
+       user ubuntu,
+       HostName 18.207.207.117,
+       IdentifyFile ~/.ssh/school
+",
+  owner   => 'root'
 }
