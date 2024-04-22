dd6f63c1b63fd17ed69acb5f6033ab0a65022315
Method Mkoma <gershommethod02@gmail.com>
2024-04-22 22:10:43 +0300

    Task 0: fixing

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,19 +16,22 @@ def req(inpt):
         lst = []
         for n in range(len(data)):
             if data[n]['userId'] == int(inpt):
-                    lst.append(data[n])
-        td = [i for i in lst if i['completed'] == True]
+                lst.append(data[n])
+        td = [i for i in lst if i['completed'] is True]
         tot = len(lst)
         td_count = len(td)
         EMPLOYEE_NAME = int(inpt)
         NUMBER_OF_DONE_TASKS = td_count
         TOTAL_NUMBER_OF_TASKS = tot
-        print(f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+        print(
+                f'Employee {EMPLOYEE_NAME} is done with ' +
+                f'tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):'
+                )
         for j in td:
             print(f"\t {j['title']}")
     except urllib.error.HTTPError as e:
         print(f'Error: {e}')
 
+
 if __name__ == "__main__":
-    l = sys.argv[1]
-    req(l)
+    req(sys.argv[1])
