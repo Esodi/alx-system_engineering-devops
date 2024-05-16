fb46052b29a377592c0619f4f946febdf57dac53
Method Mkoma <gershommethod02@gmail.com>
2024-05-16 11:23:45 +0300

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 
 exec {'modifying files limit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin',
 }
